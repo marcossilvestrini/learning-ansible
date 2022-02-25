8566f73fdbe5e9544f7df64771b305201e7a826b
marcos <marcos.silvestrini@gmail.com>
2022-02-24 22:03:46 -0300

    feat: Add new examples

--- Vagrant/Ansible/scripts/install.sh
+++ Vagrant/Ansible/scripts/install.sh
@@ -10,11 +10,16 @@ cp -f /home/vagrant/configs/.bashrc /home/vagrant
 # Set ssh
 cat /home/vagrant/security/id_rsa.pub >>/home/vagrant/.ssh/authorized_keys
 
-# Install vim
+# Install Packages
 sudo apt install -y vim
 sudo apt install -y net-tools
+sudo apt install -y git
 
 # Install Ansible
 #sudo apt install -y ansible
 sudo apt install -y python3-pip
 sudo pip3 install --no-cache-dir ansible
+
+#Clone Project Repository
+git clone https://github.com/marcossilvestrini/learning-ansible.git
+mkdir ansible
